9177b0decd28f1dab5ff229d6227d55b20da9466
Aditya Sirohi <adityasirohi7@yahoo.com>
2021-08-17 14:52:22 -0400

    new branch

---
+++ src/main/java/com/adisir/barshelf/BarShelfBetaApplication.java
@@ -0,0 +1,13 @@
+package com.adisir.barshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarShelfBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarShelfBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adisir/barshelf/BarShelfBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adisir.barshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarShelfBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
